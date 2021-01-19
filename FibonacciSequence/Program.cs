70ab5ee3650525801f5b5a209e2a576da2c49875
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2021-01-19 21:23:37 +0100

    Add project files.

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of places in the sequence");
+            int placesInSequence = int.Parse(Console.ReadLine());
+            var fs = new FibonacciSequence();
+
+            Console.WriteLine("The Fibonacci Sequence up to {0} places", placesInSequence);
+            for (int i = 0; i < placesInSequence; i++)
+            {
+                Console.WriteLine(fs.FibonacciNumbers(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
